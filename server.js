1852c97d172155db7a641f5ac0042a0b0b876511
joaoalbuquerque <joao.albuquerque@teclat.com.br>
2021-08-04 20:36:17 -0300

    Show calculus result

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 3000
+
+app.use(cors())
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+  
 
 calculateResult = (value) => {
     return value ** 2
